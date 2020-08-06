98f176a5837f066011c09da0ec92a38acd276b91
MatsunamiDaiki <lax.md0@gmail.com>
2020-08-06 18:19:45 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:images)
   end
+
+  def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:explain,:status,:delivery_cost,:area,:limit,:price,:category_id,:brand_id,images_attributes: [:image, :_destroy, :id])
 end
+
---
+++ db/migrate/20200806081341_add_brand_id_to_items.rb
@@ -0,0 +1,5 @@
+class AddBrandIdToItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :items, :brand_id, :integer
+  end
+end
