b92c7ecfae68d8449dd0e64b5082115ea36db351
kyo11 <kyosuke_0911@outlook>
2020-08-07 11:44:21 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:explain,:status,:delivery_cost,:area,:limit,:price,:category_id,:brand_id,images_attributes: [:image, :_destroy, :id])
+  end
 end
