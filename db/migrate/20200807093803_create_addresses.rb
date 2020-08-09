c55ec86657b7b1b434d3cf7f37c451ebf77465ed
issa133 <katoruge@icloud.com>
2020-08-09 16:07:08 +0900

    変更　migrate

--- db/migrate/20200807093803_create_addresses.rb
+++ db/migrate/20200807093803_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.integer :post, null: false
-      t.string :orefectures, null: false
+      t.string :prefectures, null: false
       t.string :city, null: false
       t.string :number, null: false
       t.string :building
