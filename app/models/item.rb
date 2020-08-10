68df4cfabb46c71cf274e12d89ee525e48c6e1f1
MatsunamiDaiki <lax.md0@gmail.com>
2020-08-10 13:02:22 +0900

    新規投稿画面プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_cost
+  belongs_to_active_hash :limit
+  belongs_to_active_hash :area
+  belongs_to_active_hash :status
   belongs_to :brand
   belongs_to :category
   has_many :images
