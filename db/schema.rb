410aa780088a9c33639bfdf8c466cd8cc8a68c6b
MatsunamiDaiki <lax.md0@gmail.com>
2020-08-14 12:14:13 +0900

    出品機能　マークアップ

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_12_053853) do
-
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post", null: false
@@ -44,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_08_12_053853) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-
     t.text "image", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
