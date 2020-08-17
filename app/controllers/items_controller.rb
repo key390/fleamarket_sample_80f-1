71cbe337cb98ae8d60fd4c6df138152d8830fa78
MatsunamiDaiki <lax.md0@gmail.com>
2020-08-17 16:13:48 +0900

    詳細編集　保存用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,21 +23,25 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-    @category_parent_array = Category.where(ancestry: nil)
+    @parents = Category.where(ancestry: nil)
   end
+
+  def show
+    @item = Item.find(params[:id])
+    @parents = Category.where(ancestry: nil)
+ end
 
   def get_category_children
     @category_children = Category.find_by(id: params[:parent_name], ancestry: nil).children
   end
 
+ 
 
   def get_category_grandchildren
       @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   
-  def show
-     
-  end
+  
 
   def create
     @item = Item.new(item_params)
