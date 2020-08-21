61fb5fb3106183d627676b1ef13d2c9d100f6a5d
key390 <67782828+key390@users.noreply.github.com>
2020-08-21 20:31:49 +0900

    単体テスト　モデル記述。

--- app/assets/ javascripts/items.js
+++ app/assets/ javascripts/items.js
@@ -14,13 +14,12 @@ $(document).on('turbolinks:load', ()=> {
     const html = `<img data-index="${index}" src="${url}" width="250px" height="200px">`;
     return html;
   }
-
+  
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  let fileIndex = [1,2,3,4];
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
-
   $('.hidden-destroy').hide();
 
   $('#image-box').on('change', '.js-file', function(e) {
@@ -34,12 +33,12 @@ $(document).on('turbolinks:load', ()=> {
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      // fileIndexの先頭の数字を使ってinputを作る
+      // fileIndexの先頭の数字を使ってinputを作る      
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-      if( $( '.js-file_group').length == 4){
+      if( $( '.js-file_group').length == 5){
         $('.js-file_group:last').css('display', 'none');
       }
     }
